36c732b863827c4b2b8b617e23d09d1040f8ba8e
Drajad-Kusuma-Adi <drajadkusumaadi@gmail.com>
2024-04-14 16:45:32 +0700

    feat: convert lyrics string to json

--- lyrics.php
+++ lyrics.php
@@ -7,13 +7,55 @@ include_once('./Musixmatch.php');
 $musix = new MusixLyricsApi\Musix();
 $musix->checkTokenExpire();
 
+function convertLyricsToJson($input) {
+   $input_array = [];
+   $lines = explode("\n", $input);
+
+   foreach ($lines as $line) {
+       $parts = preg_split('/\s+/', $line);
+       $timestamp = $parts[0];
+       $lyrics = trim(implode(" ", array_slice($parts, 1)));
+
+       $new_array = [
+           'timestamp' => $timestamp,
+           'lyrics' => $lyrics
+       ];
+       array_push($input_array, $new_array);
+   }
+
+   foreach ($input_array as &$val) {
+      // Remove brackets from the timestamp
+      $timestamp = trim($val['timestamp'], '[]');
+      $timestamp_parts = explode(":", $timestamp);
+
+      // Check if the timestamp has the correct format
+      if (count($timestamp_parts) == 2) {
+          list($MM, $SS) = $timestamp_parts;
+          $convertedTimestamp = intval($MM) * 60 + intval($SS);
+          $val['timestamp'] = $convertedTimestamp;
+      }
+  }
+
+   usort($input_array, function($a, $b) {
+       if (isset($a['timestamp']) && isset($b['timestamp'])) {
+           return $a['timestamp'] - $b['timestamp'];
+       }
+       return 0;
+   });
+
+   return json_encode($input_array);
+}
+
 if($type === 'default') {
 $query = urlencode($_GET['q']);
 $track_id = $musix->searchTrack($query);
 if($track_id != null) {
 $response = $musix -> getLyrics($track_id);
+$response = convertLyricsToJson($response);
    if(isset($response)) {
-      echo $response;
+      header('Content-Type: application/json');
+      echo '{ "lyrics": '.$response.' }';
+    //   echo $response;
    } else {
       echo '{
             "error":"Lyrics seems like doesn\'t exist.",
